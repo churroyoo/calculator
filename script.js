4bb39de1b7db929c2c3b812fac9015d9101b5295
HPA Macbook Pro <skcmhm@gmail.com>
2023-01-26 11:00:47 -0800

    Create strucutre + operator functions

---
+++ script.js
@@ -0,0 +1,18 @@
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function sub(num1, num2){
+    return (num1) - (num2);
+}
+function mult(num1, num2){
+    return num1 * num2;
+}
+
+function div(num1, num2){
+    return num1 / num2;
+}
+
+function operate(num1, num2, operator){
+    return operator(num1, num2);
+}
